fa22deee9c051ed3fdd932b6fb04440e798113a2
xdoo <claus.straube@gmail.com>
2020-05-18 15:33:40 +0200

    added konva

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueKonva from 'vue-konva'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueKonva)
 
 new Vue({
   vuetify,
